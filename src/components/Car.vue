dd367274cc1aa8f273e7491b5e2583079c56becc
trevorXu <xymzhs@gmail.com>
2022-08-24 16:57:11 +0800

    feat : init babylon scene

---
+++ src/components/Car.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { BabylonScene } from "../bbl";
+
+const { height, width } = defineProps<{ width: string; height: string }>();
+
+onMounted(() => {
+  const canvas = document.querySelector("canvas");
+  new BabylonScene(canvas as HTMLCanvasElement);
+});
+</script>
+
+<template>
+  <div>
+    <canvas :width="width" :height="height"></canvas>
+  </div>
+</template>
+
+<style scoped>
+div {
+  filter: drop-shadow(0 0 0.3em #646cffaa);
+}
+</style>
